01ce88c751fdceb23d2f3acda610f1a15e15e686
Santanu <santanu_cst@yahoo.com>
2022-07-14 22:19:14 +0530

    updated version

--- src/main/java/com/scode/controller/UserController.java
+++ src/main/java/com/scode/controller/UserController.java
@@ -3,7 +3,6 @@ package com.scode.controller;
 import com.scode.record.UserRecord;
 import com.scode.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +12,15 @@ import reactor.core.publisher.Flux;
 @RestController
 @RequestMapping("/api/users")
 @Tag(name = "User Reactive-API")
-@RequiredArgsConstructor
-public class UserController {
+public record UserController(UserService userService) {
 
-    private final UserService userService;
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<UserRecord> getAllStream() {
+        return userService.getAllUsers();
+    }
 
-    @GetMapping(value = "/all", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<UserRecord> getAll() {
+    @GetMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Flux<UserRecord> getAllJson() {
         return userService.getAllUsers();
     }
 
